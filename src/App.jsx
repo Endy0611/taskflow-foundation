5dc6bbc2e314ef8da273f327a0812b28fc68f74a
unknown <lonhraksmey828282@gmail.com>
2025-09-09 22:28:45 +0700

    [all 80% & on member card not ready]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import NavbarB4Login from './components/nav&footer/NavbarB4Login'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarB4Login from "./components/nav&footer/NavbarB4Login";
+import HomePage from "../src/GuestUser/HomePage.jsx";
+import FeaturesPage from "./GuestUser/FeaturePage.jsx";
+import AboutUs from "./GuestUser/AboutUs.jsx";
 
 export default function App() {
-  return (
-    <div>
-      <NavbarB4Login />
-    </div>
-  )
+    return (
+        <Router>
+            <NavbarB4Login />
+            <Routes>
+                <Route path="/home" element={<HomePage/>} />
+                <Route path="/features" element={<FeaturesPage />} />
+                <Route path="/about" element={<AboutUs />} />
+            </Routes>
+        </Router>
+    );
 }
