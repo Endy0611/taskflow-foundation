b23cd11e26466dd6e0e8561ba8f4537ad4e23ac5
Dana Dorn <dorndana8899@gmail.com>
2025-09-16 10:31:52 +0700

    root layout

---
+++ src/components/layout/RootLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavbarB4Login from '../nav&footer/NavbarB4LoginComponent'
+
+export default function RootLayout() {
+  return (
+    <div>
+      <NavbarB4Login />
+    </div>
+  )
+}
