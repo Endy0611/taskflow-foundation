b5acdc13e3b0ebe6c58121fc17148ba6cf2fb66e
Dana Dorn <dorndana8899@gmail.com>
2025-09-22 21:44:21 +0700

    add on when switch to list view

--- src/pages/template/TemplateCard.jsx
+++ src/pages/template/TemplateCard.jsx
@@ -1,5 +1,6 @@
-import { motion } from 'framer-motion';
-import "../../index.css";
+import React from "react";
+import { motion } from "framer-motion";
+import "../../index.css"
 
 export default function TemplateCard({ template }) {
   return (
