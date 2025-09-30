a1647bfc089d5434cb167e7065720e0b1b876983
Endy0611 <endyong18@gmail.com>
2025-09-30 08:05:59 +0700

    pull latest code

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from "./pages/auth/RegisterPage.jsx";
 import LoginPage from "./pages/auth/LoginPage.jsx";
+import GuestHomePage from "./pages/GuestUser/HomePage.jsx";
 import "./App.css";
 
 export default function App() {
   return (
-    <Routes>
-      {/* default -> login */}
-      <Route path="/" element={<Navigate to="/login" replace />} />
+    // <Routes>
+    //   {/* default -> login */}
+    //   <Route path="/" element={<Navigate to="/login" replace />} />
 
-      {/* public routes */}
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+    //   {/* public routes */}
+    //   <Route path="/login" element={<LoginPage />} />
+    //   <Route path="/register" element={<RegisterPage />} />
 
-      {/* catch-all -> login */}
-      <Route path="*" element={<Navigate to="/login" replace />} />
-    </Routes>
+    //   {/* catch-all -> login */}
+    //   <Route path="*" element={<Navigate to="/login" replace />} />
+    // </Routes>
+    <GuestHomePage/>
   );
 }
